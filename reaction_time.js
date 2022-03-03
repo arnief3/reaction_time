45aa1dcc876f2b61cbc17f02b4a341c38b39489a
arnief3 <arnief3@gmail.com>
2022-03-02 20:44:43 -0800

    changed to pointerdown

--- reaction_time.js
+++ reaction_time.js
@@ -49,5 +49,5 @@ window.addEventListener('keydown', function (e) {
     handle_keypress();
   }, false);
 
-window.addEventListener("mousedown", handle_keypress);
+window.addEventListener("pointerdown", handle_keypress);
 
